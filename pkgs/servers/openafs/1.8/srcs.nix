7c44a189adc92b1c94e6b145bebefd0181560f01
Michael Raitza <michael.raitza@tu-dresden.de>
2021-01-15 10:17:17 +0100

    openafs: 1.8.6 -> 1.8.7
    Fixes a serious RX protocol issue which renders servers and clients unable to
    establish communication when they were started at a date later than 0x5fffffff
    seconds since the Epoch.

--- pkgs/servers/openafs/1.8/srcs.nix
+++ pkgs/servers/openafs/1.8/srcs.nix
@@ -1,14 +1,14 @@
 { fetchurl }:
 rec {
-  version = "1.8.6";
+  version = "1.8.7";
   src = fetchurl {
     url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-src.tar.bz2";
-    sha256 = "0i99klrw00v4bwd942n90xqfn16z6337m89xfm9dgv7ih0qrsklb";
+    sha256 = "sha256-U1Q6Vh/OZ3FP7J8qa/QIxcwdBhx9ydFFlFgnXozL+nk=";
   };
 
   srcs = [ src
     (fetchurl {
       url = "https://www.openafs.org/dl/openafs/${version}/openafs-${version}-doc.tar.bz2";
-      sha256 = "1s91kmxfimhdqrz7l6jgjz72j9pyalghrvg4h384fsz0ks6s4kz3";
+    sha256 = "sha256-1hOE79zhqsq90YkAYq2nQcDgrrTMlzgRpJPc2m9KMX8=";
     })];
 }
